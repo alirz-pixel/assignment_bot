70a48276cf51a1982629100cc1eb7c514c8f2de3
alirz-pixel <74577714+alirz-pixel@users.noreply.github.com>
2021-02-22 09:49:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ async def on_ready():
 
     print(client.user.name)
     print('성공적으로 봇이 시작되었습니다.')
-    game = discord.Game('명령어 : !과제  ')
+    now = time.strftime('%c', time.localtime(time.time()))
+    game = discord.Game(now)
     await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
